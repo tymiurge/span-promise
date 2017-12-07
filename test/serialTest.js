8f55c56bf91561d7c081dc97f84b64d1258d1b31
tymiurge <tmrlabas@gmail.com>
2017-12-07 09:38:12 +0200

    TEST: serialTest - added rejected seria test case

--- test/serialTest.js
+++ test/serialTest.js
@@ -1,12 +1,10 @@
 import Promise from 'es6-promise'
 import spanPromise from '../src/index'
-/*
-  Example Unit Test
- */
 
 
 describe('serial', () => {
-  it('test that promices are executed in the predefined order', () => {
+
+  it('test that promises are executed in the predefined order', () => {
     const spans = [300, 100]
     const funcPromises = spans.map(span => () => new Promise((resolve, reject) => {
         setTimeout(() => { resolve({span, resolvedAt: Date.now() | 0}) }, span)
@@ -16,4 +14,27 @@ describe('serial', () => {
         expect(promisesExecutedInOrder).toBe(true)
     })
   })
+
+  it('test rejected seria', () => {
+    const funcPromises = [
+      () => new Promise((resolve, reject) => {
+          setTimeout(() => { resolve(300) }, 300)
+      }),
+      () => new Promise((resolve, reject) => {
+          setTimeout(() => { reject(200) }, 200)
+      }),
+      () => new Promise((resolve, reject) => {
+          setTimeout(() => { resolve(100) }, 100)
+      })
+    ]
+  
+    const result = spanPromise.serial(funcPromises)
+      .then(results => {
+          fail('then should not be called if seria is rejected.')
+      })
+      .catch(err => {
+          expect(err).toBe(200)
+      })
+  })
+  
 })
