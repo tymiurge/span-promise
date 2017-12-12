be2641a974926d2356e4f486d77d2a54cde6a39a
tymiurge <tmrlabas@gmail.com>
2017-12-12 13:51:35 +0200

    TEST: serialEvery full coverage

--- test/serialEveryTest.js
+++ test/serialEveryTest.js
@@ -4,7 +4,7 @@ import spanPromise from '../src/index'
 
 describe('serialEvery', () => {
 
-  it('serialEvery positive', () => {
+  it('serialEvery positive - check that all passed promises have passed:true', () => {
     const spans = [300, 100]
     const funcPromises = spans.map(span => () => new Promise((resolve, reject) => {
         setTimeout(() => { resolve({span, resolvedAt: Date.now() | 0}) }, span)
@@ -21,7 +21,7 @@ describe('serialEvery', () => {
         })    
   })
 
-  it('serialEvery check fails at second iteration', () => {
+  it('serialEvery check that failed promises have passed: false', () => {
       const funcPromises = [
           () => new Promise((resolve, reject) => { setTimeout(() => { resolve({span: 100}) }, 100) }),
           () => new Promise((resolve, reject) => { setTimeout(() => { resolve({span: 200}) }, 200) })
@@ -29,10 +29,41 @@ describe('serialEvery', () => {
       const serialEveryCheck = (accumulator, lastResult) => lastResult.span < 200
 
       spanPromise.serialEvery(funcPromises, serialEveryCheck).then(results => {
-          expect(results.lenght).toBe(2)
           expect(results[1].passed).toBe(false)
       })
   })
+
+  it('check that no promises are executed after a check is failed', () => {
+    const funcPromises = [
+        () => new Promise((resolve, reject) => { setTimeout(() => { resolve({span: 100}) }, 100) }),
+        () => new Promise((resolve, reject) => { setTimeout(() => { resolve({span: 200}) }, 200) }),
+        () => new Promise((resolve, reject) => { setTimeout(() => { resolve({span: 300}) }, 300) })
+    ]
+    const serialEveryCheck = (accumulator, lastResult) => lastResult.span < 150
+    
+    spanPromise.serialEvery(funcPromises, serialEveryCheck).then(results => {
+        expect(results.lenght).toBe(2)
+    })
+
+  })
+
+  it('check passed promises after a subsequent one is rejected', () => {
+    const funcPromises = [
+        () => new Promise((resolve, reject) => { setTimeout(() => { resolve({span: 100}) }, 100) }),
+        () => new Promise((resolve, reject) => { setTimeout(() => { reject('rejected') }, 200) }),
+        () => new Promise((resolve, reject) => { setTimeout(() => { resolve({span: 300}) }, 300) })
+    ] 
+    const serialEveryCheck = (accumulator, lastResult) => lastResult.span < 150
+    
+    spanPromise.serialEvery(funcPromises, serialEveryCheck)
+        .then(results => {
+            fail('then should not be called if seria is rejected.')
+        })
+        .catch(err => {
+            expect(err).toBe('rejected')
+        })
+  })
+  
   
 })
 
